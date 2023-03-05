2e47761b591c529522af38cbd1a316a5084a3f46
jason <jason.c9340@gmail.com>
2023-03-05 19:44:46 +0900

    carwash파일생성

--- app/src/main/java/com/jason/example/toyproject_carwash/LoginActivity.java
+++ app/src/main/java/com/jason/example/toyproject_carwash/LoginActivity.java
@@ -1,14 +1,20 @@
 package com.jason.example.toyproject_carwash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.jason.example.toyproject_carwash.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
     }
 }
